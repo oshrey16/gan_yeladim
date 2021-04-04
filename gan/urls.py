87fb65e55c143baaff467425ba3f73bf6d4dcd90
unknown <michalabr5@gmail.com>
2021-04-04 15:38:25 +0300

    login page fix 2

--- gan/urls.py
+++ gan/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from homePage import views
 
 urlpatterns = [
-    url(r'^homePage/', include('homePage.urls')),
+    url(r'^homePage/', views.login_success, name='login_success'),
     url(r'^ganenet/', admin.site.urls),
 	url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^parentsPage/', include('parentsPage.urls')),
--- homePage/views.py
+++ homePage/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
-def index(request):
-    return HttpResponse("Hello. This is our home page! WELCOME!")
+#def index(request):
+ #   return HttpResponse("Hello. This is our home page! WELCOME!")
+	
+
+
+def login_success(request):
+    if request.user.groups.filter(name="Parents").exists():
+        return redirect("parentsPage")
+    elif request.user.groups.filter(name="Kids").exists():
+        return redirect("kidsPage")	
+	
