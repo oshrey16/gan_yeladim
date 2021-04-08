284e9e19a9da3aad103ed67d1f3416fa25a13df6
marinahatamlasky <marinajata@gmail.com>
2021-04-08 22:23:13 +0300

    update html

--- parentsPage/urls.py
+++ parentsPage/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='parentsPage'),
+    url(r'^submissionParent', views.submissionsParent, name='submissionsParent'),
 ]
--- parentsPage/views.py
+++ parentsPage/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from homePage.models import subject
 
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     #return HttpResponse("Hello, You're at the parentsPage index.")
     return render(request,"parentsPage.html")
+
+def submissionsParent(request):
+    return render(request,"submissionsParent.html")
 
