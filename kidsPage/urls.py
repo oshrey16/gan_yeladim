2ee8c0710237a12fcd3273c84469f9590d85d8cc
michalabr5 <michalabr5@gmail.com>
2021-04-09 15:04:48 +0300

    BSPM2021T23-31

--- kidsPage/urls.py
+++ kidsPage/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='kidsPage'),
+	url(r'^(?P<subject_id>[a-zA-Z]+)/$', views.contents, name='contents'),
+	url(r'^(?P<fl_path>[a-zA-Z]+)/', views.download_file),
 ]
--- kidsPage/views.py
+++ kidsPage/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from homePage.models import subject
 
-
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     #return HttpResponse("Hello, world. You're at the kidsPage.")
     return render(request,"kidsPage.html")
+	
+def contents(request, subject_id):
+	data = subject.objects.all()
+	sub = {
+    "subject_id": data
+	}
+	return render(request,"subject.html", sub)
+	
+def download_file(request,fl_path):
+	data = subject.objects.all()
+	sub = {
+    "subject_id": data
+	}
+	fl_path = data.values('submissions')[0]['submissions']
+	filename = fl_path.split('/')[1]
+	fl = open(fl_path, 'r')
+	response = HttpResponse(fl, content_type='application/pdf')
+	response['Content-Disposition'] = "attachment; filename=%s" % filename
+	return response	
