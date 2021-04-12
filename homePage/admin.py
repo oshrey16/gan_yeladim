2fdde3b91c8438681e86a8889581c38882e0fab5
michalabr5 <michalabr5@gmail.com>
2021-04-12 19:32:51 +0300

    BSPM2021T23-167

--- homePage/admin.py
+++ homePage/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import kid, subject
+from .models import kid, subject, Question, Choice
 from parentsPage.models import submission
 
 admin.site.register(kid)
 admin.site.register(subject)
+admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(submission)
--- homePage/models.py
+++ homePage/models.py
@@ -22,3 +22,15 @@ class subject(models.Model):
 	submissions = models.FileField(upload_to='submissionsTasks')	
 	def __str__(self):
 		return self.nameSubject
+		
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.question_text
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)	
+    def __str__(self):
+        return self.choice_text
