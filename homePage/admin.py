aeb4ee4a2d35aba5eb8b5e2ad98922555cf17334
malibartal <maliba@ac.sce.ac.il>
2021-04-24 22:48:52 +0300

    BSPM2021T23-132

--- homePage/admin.py
+++ homePage/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import kid, subject, Question, Choice,Meeting,News
-from parentsPage.models import submission
+from .models import kid, subject, Question, Choice,Meeting,News,Submission
+
 
 admin.site.register(kid)
 admin.site.register(subject)
 admin.site.register(Question)
 admin.site.register(Choice)
-admin.site.register(submission)
+admin.site.register(Submission)
 admin.site.register(Meeting)
 admin.site.register(News)
--- homePage/models.py
+++ homePage/models.py
@@ -22,6 +22,12 @@ class subject(models.Model):
 	submissions = models.FileField(upload_to='submissionsTasks')	
 	def __str__(self):
 		return self.nameSubject
+
+class Submission(models.Model):
+	nameSubmission= models.CharField(max_length=200, primary_key=True)
+	
+	def __str__(self):
+		return self.nameSubmission
 # ================ #
 # model Survey
 # ================ #
