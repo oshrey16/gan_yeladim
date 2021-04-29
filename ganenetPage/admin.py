61efef107e518cd1ba7468457ee62dc543a50505
marinahatamlasky <marinajata@gmail.com>
2021-04-29 17:02:24 +0300

    BSPM2021T23-13

--- ganenetPage/admin.py
+++ ganenetPage/admin.py
@@ -2,12 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import myInfo
 
 # Register your models here.
 admin.site.register(myInfo)
-=======
-
-# Register your models here.
->>>>>>> 7f0f88e5a2987c88a1def2a3ee5a1340d523cb8a
