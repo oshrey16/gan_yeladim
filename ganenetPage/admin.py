aa83039d4b662fdcd2850f81e8f14dc010183178
michalabr5 <michalabr5@gmail.com>
2021-05-22 18:15:08 +0300

    BSPM2021T23-16

--- ganenetPage/admin.py
+++ ganenetPage/admin.py
@@ -4,5 +4,6 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import myInfo
 
+admin.site.site_url = 'http://127.0.0.1:8000/parentsPage/bugReport/'
 # Register your models here.
 admin.site.register(myInfo)
