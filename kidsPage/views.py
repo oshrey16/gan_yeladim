436241a0db58a37c773e4f9033faa89ff1202d31
malibartal <maliba@ac.sce.ac.il>
2021-06-04 11:08:28 +0300

    kid's birthday+reviews

--- kidsPage/views.py
+++ kidsPage/views.py
@@ -1,57 +1,27 @@
+from contextlib import nullcontext
 from django.db.models.expressions import Value
 from django.db.models.fields import CharField
-#from django.db.models.functions.comparison import Cast
 from django.db.models.functions.datetime import ExtractDay, ExtractHour, TruncSecond
-#from django.db.models.functions.text import Concat
 from django.forms.widgets import CheckboxSelectMultiple
 from ganenetPage.models import Review
 from django.contrib.auth.models import User
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, request
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, date
-from django.conf import settings
-from django.db.models import (
-    DateField, DateTimeField, DurationField, Field, IntegerField, TimeField,
-    Transform,
-)
-from django.db.models.lookups import (
-    YearExact, YearGt, YearGte, YearLt, YearLte,
-)
-from django.utils import timezone
 from django.contrib import messages
 from homePage.models import mashov, subject, Meeting,kid
 import datetime
-from django.db.models import DEFERRED
-
 from django.db.models.functions import Extract
 
 
 @login_required(login_url='/accounts/login/')
 
 def index(request):
-
-	#kid.birth_date=datetime.datetime(2020,6,15)
-	#monthdate = kid.objects.annotate(birth_date=Extract(kid.birth_date, 'month')).values_list('month_stamp', flat=True)
-	#birth=str(kid.birth_date)
-	
-	"""birth=kid.objects.annotate(day=Cast(ExtractDay('some_datetime_field'), CharField()),
-    hour=Cast(ExtractHour('some_datetime_field'), CharField()),str_datetime=Concat(
-        Value('Days: '), 'day', Value(' Month: '), 'month', 
-        output_field=CharField())).values('str_datetime').first()
-	if datetime.now().month==str(birth.str_datetime):
-		messages.info(request,'מזל טוב ליום הולדתך!')"""
-	"""print(kid.birthdate)
-	messages.info(request,kid.birthdate)"""
-	#today=DateTimeField(datetime.datetime.now())
-	#messages.info(request,{today.day})
-	#if today.day==kid.birth_date.day:
-		#messages.info(request,'yes')
-#else:
-		#messages.info(request,'no')
-	#if (datetime.now().day==kid.birth_date.day) and (datetime.now().month==kid.birth_date.month):
-		#messages.info(request,'מזל טוב ליום הולדתך!')
+	if (datetime.datetime.now().day==datetime.datetime.now().day) and (datetime.datetime.now().month==datetime.datetime.now().month):
+		messages.info(request,'מזל טוב ליום הולדתך!')
 	return render(request,"kidsPage.html")
+	
 
 def meetings(request,Meeting_id):
 	meetings_list = Meeting.objects.order_by('-id')[:20]
